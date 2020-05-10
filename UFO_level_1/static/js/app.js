cb1bcf21796b0fbe1947ce6f1abe96c1dd566b0a
nagam29 <mika.nagamine@gmail.com>
2020-05-10 11:47:16 -0700

    5/10/2020

--- UFO_level_1/static/js/app.js
+++ UFO_level_1/static/js/app.js
@@ -2,3 +2,35 @@
 var tableData = data;
 
 // YOUR CODE HERE!
+console.log(tableData);
+
+// Get a reference to the table body
+var tbody = d3.select('tbody');
+console.log(tbody);
+
+ // Step 4: Use d3 to append 1 cell per weather report value (weekday, date, high, low)
+ data.forEach(function(ufoData) {
+   console.log(ufoData);
+   var row = tbody.append("tr");
+
+   Object.entries(ufoData).forEach(function([key, value]) {
+     console.log(key, value);
+     // Append a cell to the row for each value
+     // in the weather report object
+     var cell = row.append("td");
+   });
+ });
+
+ // Step 5: Use d3 to update each cell's text with
+ // weather report values (weekday, date, high, low)
+ data.forEach(function(ufoData) {
+   console.log(ufoData);
+   var row = tbody.append("tr");
+   Object.entries(ufoData).forEach(function([key, value]) {
+     console.log(key, value);
+     // Append a cell to the row for each value
+     // in the weather report object
+     var cell = row.append("td");
+     cell.text(value);
+   });
+ });
