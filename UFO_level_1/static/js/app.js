0e481081576384f6ceb770c1323112f767c7e56f
nagam29 <mika.nagamine@gmail.com>
2020-05-10 22:14:56 -0700

    5/10/2020 v2

--- UFO_level_1/static/js/app.js
+++ UFO_level_1/static/js/app.js
@@ -2,35 +2,61 @@
 var tableData = data;
 
 // YOUR CODE HERE!
-console.log(tableData);
+// *** console.log(tableData);
 
 // Get a reference to the table body
 var tbody = d3.select('tbody');
-console.log(tbody);
-
- // Step 4: Use d3 to append 1 cell per weather report value (weekday, date, high, low)
- data.forEach(function(ufoData) {
-   console.log(ufoData);
-   var row = tbody.append("tr");
-
-   Object.entries(ufoData).forEach(function([key, value]) {
-     console.log(key, value);
-     // Append a cell to the row for each value
-     // in the weather report object
-     var cell = row.append("td");
-   });
- });
+// *** console.log(tbody);
 
  // Step 5: Use d3 to update each cell's text with
  // weather report values (weekday, date, high, low)
  data.forEach(function(ufoData) {
-   console.log(ufoData);
+   // *** console.log(ufoData);
    var row = tbody.append("tr");
    Object.entries(ufoData).forEach(function([key, value]) {
-     console.log(key, value);
+     // *** console.log(key, value);
      // Append a cell to the row for each value
      // in the weather report object
      var cell = row.append("td");
      cell.text(value);
    });
  });
+
+
+ // Select the button
+var button = d3.select("#filter-btn");
+
+// Select the form
+var form = d3.select("#form");
+
+// Create event handlers 
+button.on("click", runEnter);
+form.on("submit",runEnter);
+
+// button.onclick=runEnter;
+// form.onsubmit=runEnter;
+
+console.log(button);
+console.log(form);
+
+// Complete the event handler function for the form
+function runEnter() {
+  console.log("in runEnter");
+
+  // Prevent the page from refreshing
+  d3.event.preventDefault();
+  
+  // Select the input element and get the raw HTML node
+  var inputElement = d3.select("#datetime");
+  console.log(inputElement);
+
+  // Get the value property of the input element
+  var inputValue = inputElement.property("value");
+
+  console.log(inputValue);
+  //console.log(tableData);
+
+  var filteredData = tableData.filter(eventDate => tableData.datetime === inputValue);
+
+  console.log(filteredData);
+};
